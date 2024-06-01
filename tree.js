1a49405c38216eb208164de13b196c7b73296fd5
Mason Bybee <bybeemason@gmail.com>
2024-05-31 19:48:31 -0500

    added sumValues, countEvens, and numGreater methods

--- tree.js
+++ tree.js
@@ -15,20 +15,60 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (this.root) {
+      const sumValueQueue = [this.root];
+      let sum = 0;
+      while (sumValueQueue.length) {
+        const current = sumValueQueue.shift();
+        sum += current.val;
+        for (let child of current.children) {
+          sumValueQueue.push(child);
+        }
+      }
+      return sum;
+    } else return 0;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    if (this.root) {
+      const countValueQueue = [this.root];
+      let count = 0;
+      while (countValueQueue.length) {
+        const current = countValueQueue.shift();
+        if (!current.val) {
+          console.log(current.val);
+        }
+        if (current.val % 2 === 0) {
+          count += 1;
+        }
+        for (let child of current.children) {
+          countValueQueue.push(child);
+        }
+      }
+      return count;
+    } else return 0;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    if (this.root) {
+      const countValueQueue = [this.root];
+      let count = 0;
+      while (countValueQueue.length) {
+        const current = countValueQueue.shift();
+        if (current.val > lowerBound) {
+          count += 1;
+        }
+        for (let child of current.children) {
+          countValueQueue.push(child);
+        }
+      }
+      return count;
+    } else return 0;
   }
 }
 
